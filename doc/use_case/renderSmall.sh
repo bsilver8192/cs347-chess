a13a8c99f402871208bae049f63dc34514fbbe0d
Marc Gotliboym <mgotliboym@gmail.com>
2016-03-01 16:21:33 -0500

    Remove extraneous render file

--- doc/use_case/renderSmall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# This script renders the graph.
-# The Debian/Ubuntu package you need to install to get dot(1) is called
-# graphviz.
-
-cd "$(dirname "${BASH_SOURCE[0]}")"
-
-dot -Tpng fullDiagram.dot -o fullDiagram.png
